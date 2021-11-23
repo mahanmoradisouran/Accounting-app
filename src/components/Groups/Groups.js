dd7cd68507f1bc6b0c3c0ec2bf1d1274798121fc
mahanmoradi <mahan241185@gmail.com>
2021-11-23 18:12:52 +0330

    Edit on groups function completed

--- src/components/Groups/Groups.js
+++ src/components/Groups/Groups.js
@@ -17,6 +17,11 @@ const Groups = () => {
   const product = useContext(ProductProvider);
   const [value, setValue] = useState("");
   const [searchValue, setSearchValue] = useState("");
+  const [EditGroups, setEditGroup] = useState({
+    isShow: false,
+    targetGroupName: "",
+    newGroupName: "",
+  });
 
   const AddGroupHandler = (e) => {
     e.preventDefault();
@@ -84,10 +89,6 @@ const Groups = () => {
     const groups = [...group];
     setGroup(groups.filter((group) => group !== groupName));
     toast.error(groupName + " group whas deleted");
-    // setGroup();
-  };
-  const editGroupHandler = () => {
-    console.log("edited");
   };
   const serachOnGrouplistHandler = () => {
     let groups;
@@ -101,7 +102,17 @@ const Groups = () => {
               {product.filter((product) => product.group === group).length}
             </div>
             <div className={styles.buttonItem}>
-              <button onClick={editGroupHandler}>Edit</button>
+              <button
+                onClick={() =>
+                  setEditGroup({
+                    isShow: !EditGroups.isShow,
+                    targetGroupName: group,
+                    newGroupName: "",
+                  })
+                }
+              >
+                Edit
+              </button>
               <button onClick={() => deletGroupHandler(group)}>Delete</button>
             </div>
           </li>
@@ -112,7 +123,6 @@ const Groups = () => {
         group.trim().toLowerCase().includes(searchValue.trim().toLowerCase())
       );
       groups = filteredItems;
-      console.log(filteredItems, groups);
 
       return groups.map((group) => {
         return (
@@ -122,7 +132,17 @@ const Groups = () => {
               {product.filter((product) => product.group === group).length}
             </div>
             <div className={styles.buttonItem}>
-              <button onClick={editGroupHandler}>Edit</button>
+              <button
+                onClick={() =>
+                  setEditGroup({
+                    isShow: !EditGroups.isShow,
+                    targetGroupName: group,
+                    newGroupName: "",
+                  })
+                }
+              >
+                Edit
+              </button>
               <button onClick={() => deletGroupHandler(group)}>Delete</button>
             </div>
           </li>
@@ -130,53 +150,118 @@ const Groups = () => {
       });
     }
   };
+  const showEditPopup = ({ targetGroupName }) => {
+    const changeGroupName = () => {
+      if (EditGroups.newGroupName === "") {
+        toast.error("Please enter some value");
+      } else if (EditGroups.newGroupName === targetGroupName) {
+        toast.error("Please enter a new name. You cant enter previos name");
+      } else {
+        const groups = [...group];
+        const filteredGroup = groups.findIndex(
+          (group) => group === targetGroupName
+        );
+        groups[filteredGroup] = EditGroups.newGroupName;
+        toast.success(
+          "The group " +
+            targetGroupName +
+            " was successfuly changed to " +
+            EditGroups.newGroupName
+        );
+        setEditGroup({
+          isShow: false,
+          targetGroupName: EditGroups.targetGroupName,
+          newGroupName: EditGroups.newGroupName,
+        });
+        setGroup(groups);
+      }
+    };
+
+    return (
+      <>
+        <div className={styles.popup}>
+          <div
+            onClick={() => setEditGroup(false)}
+            className={styles.overlay}
+          ></div>
+          <div className={styles.editGroupContainer}>
+            <div className={styles.title}>
+              <h2>Edite groupName</h2>
+              <button onClick={() => setEditGroup(false)}>X</button>
+            </div>
+            <div className={styles.body}>
+              <label htmlFor="newGroup_input">Enter new Group name</label>
+              <input
+                type="text"
+                id="newGroup_input"
+                value={EditGroups.newGroupName}
+                onChange={(e) =>
+                  setEditGroup({
+                    isShow: EditGroups.isShow,
+                    targetGroupName: EditGroups.targetGroupName,
+                    newGroupName: e.target.value,
+                  })
+                }
+              />
+            </div>
+          </div>
+          <button onClick={changeGroupName} className={styles.changeButton}>
+            change
+          </button>
+        </div>
+      </>
+    );
+  };
 
   return (
-    <div className={styles.GroupsContainer}>
-      <Toaster />
-      <h2>Recently added</h2>
-      <div className={styles.GroupsListContainer}>
-        <ul>{showRecentHandler()}</ul>
-      </div>
-      <h2>Add new group</h2>
-      <div className={styles.FormContainer}>
-        <form onSubmit={AddGroupHandler} className={styles.Form}>
-          <label>Enter group name</label>
-          <div>
-            <input
-              onChange={(e) => setValue(e.target.value)}
-              value={value}
-              type="text"
-            />
-            <button type="submit">
-              <AiFillFolderAdd />
-            </button>
-          </div>
-        </form>
-      </div>
-      <h2>Delete and edit group</h2>
-      <div className={styles.DeletFormContainer}>
-        <ul className={styles.DeletForm}>
-          <div className={styles.searchForm}>
-            <label htmlFor="delet_input">Search in groups</label>
-            <input
-              type="text"
-              id="delet_input"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-          </div>
-          <ul className={styles.groupList}>
-            <li className={styles.groupListTitle}>
-              <div>Group name</div>
-              <div>Length</div>
-              <div>Edit groups and Delete </div>
-            </li>
-            {serachOnGrouplistHandler()}
+    <>
+      {EditGroups.isShow && showEditPopup(EditGroups)}
+      <div className={styles.GroupsContainer}>
+        <Toaster />
+        <h2>Recently added</h2>
+        <div className={styles.GroupsListContainer}>
+          <ul>{showRecentHandler()}</ul>
+        </div>
+        <h2>Add new group</h2>
+        <div className={styles.FormContainer}>
+          <form onSubmit={AddGroupHandler} className={styles.Form}>
+            <label>Enter group name</label>
+            <div>
+              <input
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+                type="text"
+              />
+              <button type="submit">
+                <AiFillFolderAdd />
+              </button>
+            </div>
+          </form>
+        </div>
+        <h2>Delete and edit group</h2>
+        <div className={styles.DeletFormContainer}>
+          <ul className={styles.DeletForm}>
+            <div className={styles.searchForm}>
+              <label htmlFor="delet_input">Search in groups</label>
+              <input
+                type="text"
+                id="delet_input"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            </div>
+            <ul className={styles.groupList}>
+              <li className={styles.groupListTitle}>
+                <div>Group name</div>
+                <div>Length</div>
+                <div>Edit groups and Delete </div>
+              </li>
+              {serachOnGrouplistHandler()}
+            </ul>
           </ul>
-        </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
